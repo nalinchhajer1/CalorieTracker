dae6b689e6ed57814a1b028da205d156f4d7b8ec
Nalin Chhajer <nalinchhajer@gmail.com>
2022-06-07 18:08:52 +0530

    Fixing test setup

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const RootStack = createNativeStackNavigator();
 export default function App() {
-
-  return <NavigationContainer>
-    <RootStack.Navigator>
-      <RootStack.Screen name="Home" component={HomeScreen} />
-    </RootStack.Navigator>
-  </NavigationContainer>
-
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Home" component={HomeScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 function HomeScreen() {
   return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
   );
 }
 
